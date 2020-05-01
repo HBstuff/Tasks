785b6d22ffe05d7519a2c732040c2edb08249316
HBstuff <61944568+HBstuff@users.noreply.github.com>
2020-05-01 22:33:40 +0300

    Create car.py

---
+++ car.py
@@ -0,0 +1,7 @@
+class Car:
+        def __init__(self, make, model, mileage, price, feats):
+                self.make = make
+                self.model = model
+                self.mileage = mileage
+                self.price = price
+                self.feats = feats
